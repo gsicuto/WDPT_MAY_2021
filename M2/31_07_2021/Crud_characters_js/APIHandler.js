d2e4dc55f455dc0912df1165b81d6fe207f17ab6
Gabriel Sicuto <gabrielsicuto@gmail.com>
2021-08-02 08:55:23 -0300

    aula 31/07/2021

---
+++ M2/31_07_2021/Crud_characters_js/APIHandler.js
@@ -0,0 +1,42 @@
+class APIHandler {
+  constructor (baseUrl) {
+    this.BASE_URL = baseUrl;
+  }
+
+  getFullList = async () => {
+    const result = await axios.get(`${this.BASE_URL}/characters`)
+    return result.data
+
+  }
+
+  getOneRegister = async (id) => {
+    const result = await axios.get(`${this.BASE_URL}/characters/${id}`)
+    return result.data
+  }
+
+  createOneRegister = async (newCharacter) => {
+    console.log(newCharacter)
+    if (!newCharacter.name || !newCharacter.occupation || !newCharacter.weapon){
+      throw 'Missing Fields'
+    }
+    if(!newCharacter.hasOwnProperty('cartoon')){
+      newCharacter.cartoon = false
+    }
+    const result = await axios.post(`${this.BASE_URL}/characters`, newCharacter)
+    return result.data
+  }
+
+  updateOneRegister =  async (id, updatedCharacter) => {
+    try {
+      const result = await axios.put(`${this.BASE_URL}/characters/${id}`, updatedCharacter)
+      return result.data
+    } catch (error) {
+      throw error
+    }
+  }
+
+  deleteOneRegister = async (id) => {
+    const result = await axios.delete(`${this.BASE_URL}/characters/${id}`)
+    return result.data
+  }
+}
