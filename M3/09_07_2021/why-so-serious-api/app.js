ed187ba8369c183e5a0f81abd9cd9956cc3a7329
Gabriel Sicuto <gabrielsicuto@gmail.com>
2021-09-11 11:56:51 -0300

    aula 11/09/2021

--- M3/09_07_2021/why-so-serious-api/app.js
+++ M3/09_07_2021/why-so-serious-api/app.js
@@ -36,4 +36,36 @@ app.get('/jokes', async (req, res) => {
   }
 });
 
+app.get('/joke/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const joke = await Joke.findById(id);
+    res.status(200).json(joke);
+  } catch (error) {
+    res.status(500).json({ msg: 'Error on find joke', error });
+  } 
+});
+
+app.put('/joke/:id', async (request, response) => {
+  const { id } = request.params;
+  const payload = request.body;
+  try {
+    const updatedJoke = await Joke.findOneAndUpdate({ _id: id }, payload, { new: true });
+    response.status(200).json(updatedJoke);
+  } catch (error) {
+    response.status(500).json({ msg: 'Error while updating joke', error });
+  }
+});
+
+app.delete('/joke/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Joke.findByIdAndDelete(id);
+    res.status(204).json();
+  } catch (error) {
+    res.status(500).json({ msg: 'Error while deleting joke', error });
+  }
+ });
+
+
 app.listen(PORT, () => console.log(`Server listen on Port ${PORT}`));
